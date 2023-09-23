444c1a5ddcdd08ebfcfd2a4bf8274aee9d440fe7
음지훈 <jhmc5268@naver.com>
2023-09-23 22:01:36 +0900

    Feat: kakaomap 초기 좌표 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 
 import Map from './Map';
+import axios from 'axios';
 
 
 declare global {
@@ -11,31 +12,7 @@ declare global {
 }
 
 const App = () => {
-  // const [_map, setMap ] = useState<any>();
-
-
-
-  // useEffect(() => {
-  //   let container = document.getElementById('map'); 
-
-  //   let options = { 
-  //     center: new window.kakao.maps.LatLng(36.550701, 127.370667), 
-  //     level: 12
-  //   };
-
-  //   let map = new window.kakao.maps.Map(container, options); 
-  //   setMap(map)
-
-  // }, [])
-
-  // const onClickZoomInMap = () =>{
-  //   _map?.setLevel(_map.getLevel() - 1);
-  // }
   
-  // const onClickZoomOutMap = () =>{
-  //   _map?.setLevel(_map.getLevel() + 1);
-  // }
-
   return (
     <Continer>
       <Map />
--- src/Map.tsx
+++ src/Map.tsx
@@ -14,13 +14,13 @@ const Map = () => {
 
     useEffect(() => {
 
-    let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    let options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(37.050701, 128.570667), //지도의 중심좌표.
-      level: 12 //지도의 레벨(확대, 축소 정도)
+    let container = document.getElementById('map'); 
+    let options = {
+      center: new window.kakao.maps.LatLng(37.050701, 127.570667), 
+      level: 12
     };
 
-    let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    let map = new window.kakao.maps.Map(container, options);
     setMap(map)
     }, [])
 
@@ -49,6 +49,7 @@ export default Map
 const Continer = styled.div`
     width: 31.25rem;
     height: 43.75rem;
+    border-radius: .375rem;
     position: relative;
 `;
 
